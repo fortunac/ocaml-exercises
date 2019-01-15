5ba24207368cfb53d5942f45d154e3c2280110a7
Chloe Fortuna <cfortuna@draper.com>
2019-01-15 15:58:57 -0500

    Update Tree.ml

--- functor-practice/Tree.ml
+++ functor-practice/Tree.ml
@@ -1,14 +1,18 @@
 open !Core
 
-module type OrderedType =
-  sig
-    type t
-    val compare: t -> t -> int
-    val sexp_of_t: t -> Ppx_sexp_conv_lib.Sexp.t
-    val t_of_sexp: Ppx_sexp_conv_lib.Sexp.t -> t
-  end
+module type T = sig
+  module OrdSet : Set.S
+  type elt
+  type t
+  val compare : t -> t -> int
+  val empty : t
+  val add : elt -> t -> t
+  val at_depth : t -> int -> OrdSet.t
+end
 
-module T (Ord: OrderedType) = struct
+module Make (Ord : Set.Elt) = struct
+
+  module OrdSet = Set.Make(Ord)
 
   type elt = Ord.t
 
@@ -16,8 +20,8 @@ module T (Ord: OrderedType) = struct
     | Null
     | Node of elt * t * t
 
-  let compare x y =
-    match x, y with
+  let compare (x : t) (y : t) : int =
+    match (x, y) with
     | Null,           Null           -> 0
     | Null,           Node _         -> -1
     | Node _,         Null           -> 1
@@ -25,29 +29,28 @@ module T (Ord: OrderedType) = struct
 
   let empty = Null
 
-  let rec add x tree =
+  let rec add (x : elt) (tree : t) : t =
     match tree with
     | Null -> Node (x, Null, Null)
     | Node (y, l, r) as node ->
-        if (Ord.compare x y) < 0 then
+        let comparison = Ord.compare x y in
+        if comparison < 0 then
           Node (y, add x l, r)
-        else if (Ord.compare x y) > 0 then
+        else if comparison > 0 then
           Node (y, l, add x r)
-        else
-          (* no duplicate nodes *)
+        else (* no duplicate nodes *)
           node
 
-  let at_depth t depth =
-    let module OrdSet = Set.Make(Ord) in
-    let rec traverse t set level =
-      match t with
-      | Null           -> set
+  let at_depth (tree : t) (depth : int) : OrdSet.t =
+    let rec traverse tree set level =
+      match tree with
+      | Null -> set
       | Node (x, l, r) ->
           if level = depth then
             OrdSet.add set x
           else
             traverse l (traverse r set (level + 1)) (level + 1)
-    in traverse t OrdSet.empty 1
+    in
+    traverse tree OrdSet.empty 1
 
 end
-
--- functor-practice/Tree.mli
+++ functor-practice/Tree.mli
@@ -1,41 +1,28 @@
-(** data types that can be ordered *)
-module type OrderedType =
+open !Core
+
+module type T =
   sig
-    (** type of elements *)
+    (** set of ordered elements *)
+    module OrdSet : Set.S
+
+    (** type of ordered elements *)
+    type elt
+
+    (** the tree type *)
     type t
 
-    (** compare the order of two order types *)
+    (** compare two trees *)
     val compare : t -> t -> int
 
-    (** sexp of the ordered type *)
-    val sexp_of_t : t -> Ppx_sexp_conv_lib.Sexp.t
+    (** the empty tree *)
+    val empty : t
 
-    (** generates ordered type from sexp *)
-    val t_of_sexp : Ppx_sexp_conv_lib.Sexp.t -> t
+    (** adds an element into the tree in order *)
+    val add : elt -> t -> t
+
+    (** a set of elt at a certain depth of the tree *)
+    val at_depth : t -> int -> OrdSet.t
   end
 
-(** Binary tree over the order types *)
-module T :
-  functor (Ord : OrderedType) ->
-    sig
-      (** type of ordered elements *)
-      type elt
-
-      (** the tree type *)
-      type t
-
-      (** compare two trees *)
-      val compare : t -> t -> int
-
-      (** the empty tree *)
-      val empty : t
-
-      (** adds an element into the tree in order *)
-      val add : elt -> t -> t
-
-      (** a set of elt at a certain depth of the tree *)
-      val at_depth :
-        t ->
-        int ->
-        (elt, Core_kernel__Set.Make(Ord).Elt.comparator_witness) Base.Set.t
-    end
+module Make (Ord : Set.Elt) :
+  T with type elt = Ord.t and type OrdSet.Elt.t = Ord.t
